3285e2eed37eed9a1875e498da8d4751f99eefd7
Valentin Pomilio <pomiliovalentin218@gmail.com>
2026-04-12 20:43:42 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,138 @@
+users = [{"id":1,"first_name":"Aldric","last_name":"Stango","bank":"FIRST STATE BANK","city":"Gaoping","country":"China","salary":"$565.06","expenses":"$595.38"},
+{"id":2,"first_name":"Iolande","last_name":"Sazio","bank":"SECURITY BANK","city":"Austin","country":"United States","salary":"$389.89","expenses":"$972.73"},
+{"id":3,"first_name":"Jackie","last_name":"Waller-Bridge","bank":"Sparkasse im Landkreis Cham","city":"Spasskoye-Lutovinovo","country":"Russia","salary":"$133.97","expenses":"$653.15"},
+{"id":4,"first_name":"Belicia","last_name":"Grabbam","bank":"FIRST BANK","city":"Tawan tawan","country":"Philippines","salary":"$886.69","expenses":"$312.51"},
+{"id":5,"first_name":"Wilt","last_name":"Jakubiak","bank":"Sparkasse Chemnitz","city":"Zhongluotan","country":"China","salary":"$544.33","expenses":"$851.33"},
+{"id":6,"first_name":"Talbert","last_name":"Picton","bank":"BMO HARRIS BANK","city":"São João","country":"Portugal","salary":"$1175.18","expenses":"$486.67"},
+{"id":7,"first_name":"Natasha","last_name":"Brounfield","bank":"Spreewaldbank eG","city":"Novovasylivka","country":"Ukraine","salary":"$1216.74","expenses":"$575.74"},
+{"id":8,"first_name":"Ritchie","last_name":"Suthworth","bank":"COMMUNITY BANK","city":"Listvyanskiy","country":"Russia","salary":"$1815.29","expenses":"$617.20"},
+{"id":9,"first_name":"Anna-diana","last_name":"Tedridge","bank":"Spar- und Kreditbank Bühlertal eG","city":"Cruz del Eje","country":"Argentina","salary":"$1913.48","expenses":"$545.23"},
+{"id":10,"first_name":"Bridgette","last_name":"Ram","bank":"BMO HARRIS BANK","city":"Zall-Dardhë","country":"Albania","salary":"$585.72","expenses":"$968.99"},
+{"id":11,"first_name":"Gasper","last_name":"McMonnies","bank":"BUSEY BANK","city":"Grammatikó","country":"Greece","salary":"$1283.26","expenses":"$891.27"},
+{"id":12,"first_name":"Gui","last_name":"Zammitt","bank":"BANK IM BISTUM ESSEN eG","city":"Sammatti","country":"Finland","salary":"$1838.27","expenses":"$272.03"},
+{"id":13,"first_name":"Butch","last_name":"Delhanty","bank":"DEUTSCHE BANK AKTIENGESELLSCHAFT","city":"Töreboda","country":"Sweden","salary":"$1207.76","expenses":"$929.46"},
+{"id":14,"first_name":"Mose","last_name":"Blaasch","bank":"WESBANCO BANK INC","city":"Jiyang","country":"China","salary":"$1701.88","expenses":"$135.57"},
+{"id":15,"first_name":"Waneta","last_name":"Kenworthey","bank":"BMO HARRIS BANK","city":"Yangqiao","country":"China","salary":"$1418.35","expenses":"$333.16"},
+{"id":16,"first_name":"Dene","last_name":"Moran","bank":"CHEMICAL BANK","city":"He’an","country":"China","salary":"$1456.29","expenses":"$869.19"},
+{"id":17,"first_name":"Jozef","last_name":"Kohter","bank":"UNITED COMMUNITY BANK","city":"Mueang Phuket","country":"Thailand","salary":"$269.68","expenses":"$914.21"},
+{"id":18,"first_name":"Flem","last_name":"Attrie","bank":"FIRST FINANCIAL BANK","city":"Wutong","country":"China","salary":"$633.79","expenses":"$76.75"},
+{"id":19,"first_name":"Harri","last_name":"Roux","bank":"BANCO BPM SOCIETA' PER AZIONI","city":"Xinyi","country":"China","salary":"$1911.07","expenses":"$112.19"},
+{"id":20,"first_name":"Bev","last_name":"Wallbank","bank":"BANCA NAZIONALE DEL LAVORO S.P.A. (IN FORMA CONTRATTA BNL S.P.A.)","city":"Montgomery","country":"United States","salary":"$1765.82","expenses":"$338.67"},
+{"id":21,"first_name":"Rozanne","last_name":"Hedden","bank":"Raiffeisenbank Region Gallneukirchen eGen","city":"Rego","country":"Portugal","salary":"$128.19","expenses":"$761.74"},
+{"id":22,"first_name":"Bert","last_name":"Clemerson","bank":"Raiffeisenbank Holzkirchen-Otterfing eG","city":"Bryukhovychi","country":"Ukraine","salary":"$100.11","expenses":"$946.73"},
+{"id":23,"first_name":"Effie","last_name":"Sockell","bank":"BANQUE NAGELMACKERS","city":"Qilin","country":"China","salary":"$31.93","expenses":"$247.69"},
+{"id":24,"first_name":"Oby","last_name":"Sandeson","bank":"UNION STATE BANK","city":"Borino","country":"Bulgaria","salary":"$92.65","expenses":"$538.95"},
+{"id":25,"first_name":"Ned","last_name":"Manueli","bank":"CITIBANK","city":"Capão da Canoa","country":"Brazil","salary":"$809.63","expenses":"$157.53"},
+{"id":26,"first_name":"Waldemar","last_name":"Daoust","bank":"FIFTH THIRD BANK","city":"Reguenga","country":"Portugal","salary":"$1177.67","expenses":"$428.61"},
+{"id":27,"first_name":"Shurlocke","last_name":"Terrazzo","bank":"M B FINANCIAL BANK","city":"Dabao","country":"China","salary":"$383.63","expenses":"$458.57"},
+{"id":28,"first_name":"Aguie","last_name":"Crosthwaite","bank":"COMMUNITY STATE BANK","city":"Borehbangle","country":"Indonesia","salary":"$639.05","expenses":"$425.18"},
+{"id":29,"first_name":"Annette","last_name":"Hazeldean","bank":"BANCA DI CREDITO COOPERATIVO DI NETTUNO SOCIETA' COOPERATIVA","city":"Kalashnikovo","country":"Russia","salary":"$362.79","expenses":"$541.46"},
+{"id":30,"first_name":"Francoise","last_name":"Sitwell","bank":"GLACIER BANK","city":"Bugang","country":"Philippines","salary":"$74.40","expenses":"$559.96"},
+{"id":31,"first_name":"Pippa","last_name":"Overal","bank":"CITIBANK","city":"Fort Lauderdale","country":"United States","salary":"$1117.47","expenses":"$981.15"},
+{"id":32,"first_name":"Orville","last_name":"Tomankiewicz","bank":"PINNACLE BANK","city":"Pinhal de Frades","country":"Portugal","salary":"$40.27","expenses":"$627.45"},
+{"id":33,"first_name":"Hildagard","last_name":"Vaune","bank":"BANCO BPM SOCIETA' PER AZIONI","city":"Santo Domingo","country":"Nicaragua","salary":"$98.98","expenses":"$291.07"},
+{"id":34,"first_name":"Lion","last_name":"Lambkin","bank":"mBank Hipoteczny S.A.","city":"Grenoble","country":"France","salary":"$137.27","expenses":"$18.65"},
+{"id":35,"first_name":"Essa","last_name":"Maris","bank":"Kreissparkasse Heidenheim","city":"Buíque","country":"Brazil","salary":"$384.86","expenses":"$1.91"},
+{"id":36,"first_name":"Darrel","last_name":"Mucklow","bank":"BMO HARRIS BANK","city":"Massaranduba","country":"Brazil","salary":"$448.98","expenses":"$210.58"},
+{"id":37,"first_name":"Byrle","last_name":"Ropcke","bank":"BANCA DI CREDITO COOPERATIVO DI OSTRA E MORRO D'ALBA - SOCIETA' COOPERATIVA","city":"La Puerta de San José","country":"Argentina","salary":"$127.25","expenses":"$745.34"},
+{"id":38,"first_name":"Roland","last_name":"Kummerlowe","bank":"Cofidis","city":"Tlogowetan","country":"Indonesia","salary":"$177.08","expenses":"$503.15"},
+{"id":39,"first_name":"Aldon","last_name":"Scroggie","bank":"Lisduggan District Credit Union Limited","city":"Palmar de Varela","country":"Colombia","salary":"$366.31","expenses":"$514.43"},
+{"id":40,"first_name":"Hastie","last_name":"Hillborne","bank":"CAPITAL BANK","city":"La Cruz","country":"Argentina","salary":"$1518.59","expenses":"$394.33"},
+{"id":41,"first_name":"Janeen","last_name":"Beacock","bank":"Crédit Agricole S.A.","city":"Mbumi","country":"Tanzania","salary":"$1074.59","expenses":"$921.45"},
+{"id":42,"first_name":"Jackquelin","last_name":"Ellingford","bank":"HERITAGE BANK","city":"Três Passos","country":"Brazil","salary":"$764.71","expenses":"$122.50"},
+{"id":43,"first_name":"Lucina","last_name":"Humpage","bank":"BANCO BPM SOCIETA' PER AZIONI","city":"Tinambacan","country":"Philippines","salary":"$203.01","expenses":"$5.54"},
+{"id":44,"first_name":"Rouvin","last_name":"Ferry","bank":"BANCO BPI S.A.","city":"Bondy","country":"France","salary":"$1097.65","expenses":"$565.86"},
+{"id":45,"first_name":"Amity","last_name":"Garlinge","bank":"FIRST NATIONAL BANK","city":"Vila Ribeiro","country":"Portugal","salary":"$326.42","expenses":"$447.09"},
+{"id":46,"first_name":"Van","last_name":"Whatham","bank":"COMPASS BANK","city":"Voždovac","country":"Serbia","salary":"$508.36","expenses":"$963.64"},
+{"id":47,"first_name":"Claus","last_name":"Marchi","bank":"BANCA AGRICOLA POPOLARE DI SICILIA SOCIETA' COOPERATIVA PER AZIONI","city":"North Shore","country":"New Zealand","salary":"$1949.27","expenses":"$551.41"},
+{"id":48,"first_name":"Hadlee","last_name":"Magrannell","bank":"PROSPERITY BANK","city":"Yongping","country":"China","salary":"$200.28","expenses":"$197.16"},
+{"id":49,"first_name":"Perrine","last_name":"Mawford","bank":"BANCO BPM SOCIETA' PER AZIONI","city":"Karagandy","country":"Kazakhstan","salary":"$712.21","expenses":"$164.09"},
+{"id":50,"first_name":"Alva","last_name":"Clifford","bank":"CACEIS Bank","city":"Rabāţ-e Sangī-ye Pā’īn","country":"Afghanistan","salary":"$689.10","expenses":"$203.29"}]
+
+function balance_mensual(usuarios, persona){
+    let encontrar = false;
+    for (let i = 0; i < usuarios.length; i++){
+        if (usuarios[i].first_name.toLowerCase() === persona){
+            console.log('Nombre: ' + usuarios[i].first_name)
+            console.log('Banco: ' + usuarios[i].bank)
+
+            let salario = parseFloat(usuarios[i].salary.replace('$', ''))
+            let expensas = parseFloat(usuarios[i].expenses.replace('$', ''))
+            let ahorro = salario - expensas
+
+            console.log('Ahorro mensual: ' + ahorro)
+            clasificacion_financiera(ahorro)
+            encontrar = true
+            break;
+        }
+    }
+    if (encontrar == false){
+        console.log('No se encontro a nadie con el nombre ingresado!')
+    }
+}
+
+function clasificacion_financiera(sobrante){
+    switch (true) {
+        case sobrante <= 500:
+            console.log('Ahorro bajo');
+            break;
+        case sobrante <= 1500:
+            console.log('Ahorro medio');
+            break;
+        default:
+            console.log('Ahorro alto');
+    }
+}
+
+function agrupar_bancos(usuarios){
+    return conteoBancos = usuarios.reduce((acc, user) => {
+        let banco = user.bank;
+
+        let salario = parseFloat(user.salary.replace('$', ''));
+        let gastos = parseFloat(user.expenses.replace('$', ''));
+        let ahorro = salario - gastos;
+
+        if (!acc[banco]) {
+            acc[banco] = {
+                cantidad: 0,
+                ahorroTotal: 0
+            };
+        } 
+        acc[banco].cantidad++;
+        acc[banco].ahorroTotal += ahorro;
+        
+        return acc;
+    }, {});
+}
+
+function agrupar_pais(usuarios){
+    return usuarios.reduce((acc, user) => {
+
+        let pais = user.country;
+
+        let salario = parseFloat(user.salary.replace('$', ''));
+        let gastos = parseFloat(user.expenses.replace('$', ''));
+        let ahorro = salario - gastos;
+
+        if (!acc[pais]) {
+            acc[pais] = {
+                cantidad: 0,
+                ahorroTotal: 0
+            };
+        }
+
+        acc[pais].cantidad++;
+        acc[pais].ahorroTotal += ahorro;
+
+        return acc;
+    }, {});
+}
+
+
+let buscar_persona = balance_mensual(users, prompt("Ingresa el nombre de una persona").toLowerCase())
+let bancos_agrupados = agrupar_bancos(users)
+let paises_agrupados = agrupar_pais(users)
+console.log('= = = BANCOS AGRUPADOS = = =')
+console.log(bancos_agrupados)
+console.log('= = = PAISES AGRUPADOS = = =')
+console.log(paises_agrupados)
